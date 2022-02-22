398bf703322791338055c462b55476815adf5613
MatS <44228468+MatSzyDev@users.noreply.github.com>
2022-02-22 19:12:39 +0100

    Add files via upload

---
+++ Zaliczenie 1/Zadanie 2/dynastia01.pl
@@ -0,0 +1,36 @@
+osoba('Kazimierz II Sprawiedliwy',1194).
+osoba('Leszek Bia³y',1227).
+osoba('Konrad I Mazowiecki', 1247).
+osoba('Boles³aw', 1248).
+osoba('Kazimierz I',1267).
+osoba('Siemowit',1262).
+osoba('Leszek Czarny',1288).
+osoba('Siemomys³',1287).
+osoba('W³adyws³aw I £okietek', 1333).
+osoba('Kazimierz II',1294).
+osoba('Siemowit',1312).
+osoba('Leszek',1340).
+osoba('Przemys³',1339).
+osoba('Kazimierz III',1350).
+osoba('W³adys³aw Bia³y',1388).
+osoba('Kazimierz III Wielki',1370).
+osoba('W³adys³aw Garbaty', 1351).
+osoba('Boles³aw',1328).
+
+rodzina('Kazimierz II Sprawiedliwy','Leszek Bia³y').
+rodzina('Kazimierz II Sprawiedliwy','Konrad I Mazowiecki').
+rodzina('Konrad I Mazowiecki','Boleslaw').
+rodzina('Konrad I Mazowiecki','Kazimierz I').
+rodzina('Konrad I Mazowiecki','Siemowit').
+rodzina('Kazimierz I','Leszek Czarny').
+rodzina('Kazimierz I','Siemomys³').
+rodzina('Kazimierz I','W³adyws³aw I £okietek').
+rodzina('Kazimierz I','Kazimierz II').
+rodzina('Kazimierz I','Siemowit').
+rodzina('Siemomys³','Leszek').
+rodzina('Siemomys³','Przemys³').
+rodzina('Siemomys³','Kazimierz').
+rodzina('Kazimierz III','W³adys³aw Bia³y').
+rodzina('W³adys³aw I £okietek', 'Kazimierz III Wielki').
+rodzina('Siemowit','W³adys³aw Garbaty').
+rodzina('Siemowit','Boles³aw').
